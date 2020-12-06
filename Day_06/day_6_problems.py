0aa5590154edc0ba6f4afed60137153e96f2f7b5
GlassToeStudio <GlassToeStudio@gmail.com>
2020-12-06 09:37:04 -0600

    Add problem 2

--- Day_06/day_6_problems.py
+++ Day_06/day_6_problems.py
@@ -62,11 +62,77 @@ What is the sum of those counts?
 """
 
 
-def format_data(data):
-    yeses = sum([len(set(x.replace('\n', ''))) for x in data.read().split('\n\n')])
+"""
+--- Part Two ---
+As you finish the last group's customs declaration, you notice that you
+misread one word in the instructions:
+
+You don't need to identify the questions to which anyone answered "yes"; you
+need to identify the questions to which everyone answered "yes"!
+
+Using the same example as above:
+
+    abc
+
+    a
+    b
+    c
+
+    ab
+    ac
+
+    a
+    a
+    a
+    a
+
+    b
+
+This list represents answers from five groups:
+
+    In the first group, everyone (all 1 person) answered "yes" to 3 questions:
+    a, b, and c.
+    In the second group, there is no question to which everyone answered "yes".
+    In the third group, everyone answered yes to only 1 question, a. Since some
+    people did not answer "yes" to b or c, they don't count.
+    In the fourth group, everyone answered yes to only 1 question, a.
+    In the fifth group, everyone (all 1 person) answered "yes" to 1 question,
+    b.
+
+In this example, the sum of these counts is 3 + 0 + 1 + 1 + 1 = 6.
+
+For each group, count the number of questions to which everyone answered "yes".
+What is the sum of those counts?
+"""
+
+
+def format_data_1(data):
+    yeses = sum(
+        [len(set(x.replace('\n', ''))) for x in data.read().split('\n\n')]
+        )
     return yeses
+
+
+def format_data_2(data):
+    groups = [
+        [len(x.split('\n')), x.replace('\n', ''),
+            set(x.replace('\n', ''))]for x in data.read().split('\n\n')
+        ]
+    return groups
+
+
+def find_all_yesses(data):
+    sum = 0
+    for people, answers, sets in groups:
+        for answer in sets:
+            sum += 1 if answers.count(answer) == people else 0
+    return (sum)
 
 
 if __name__ == "__main__":
     with open("Day_06/input.txt", "r") as in_file:
-        print(f"Part 1: {format_data(in_file)}")
+        print(f"Part 1: {format_data_1(in_file)}")
+
+    with open("Day_06/input.txt", "r") as in_file:
+        groups = format_data_2(in_file)
+        print(f"Part 2: {find_all_yesses(groups)}")
