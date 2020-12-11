f907188a16b91b4292716455ded926513401748b
GlassToeStudio <GlassToeStudio@gmail.com>
2020-12-10 19:34:53 -0600

    Add day 10 part 2
    Cleaned up everything else a little.

--- Day_10/day_10_problems.py
+++ Day_10/day_10_problems.py
@@ -113,6 +113,78 @@ Find a chain that uses all of your adapters to connect the charging outlet to
 your device's built-in adapter and count the joltage differences between the
 charging outlet, the adapters, and your device. What is the number of 1-jolt
 differences multiplied by the number of 3-jolt differences?
+
+* 2312
+"""
+
+
+"""
+--- Part Two ---
+To completely determine whether you have enough adapters, you'll need to
+figure out how many different ways they can be arranged. Every arrangement
+needs to connect the charging outlet to your device. The previous rules about
+when adapters can successfully connect still apply.
+
+The first example above (the one that starts with 16, 10, 15) supports the
+following arrangements:
+
+    (0), 1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19, (22)
+    (0), 1, 4, 5, 6, 7, 10, 12, 15, 16, 19, (22)
+    (0), 1, 4, 5, 7, 10, 11, 12, 15, 16, 19, (22)
+    (0), 1, 4, 5, 7, 10, 12, 15, 16, 19, (22)
+    (0), 1, 4, 6, 7, 10, 11, 12, 15, 16, 19, (22)
+    (0), 1, 4, 6, 7, 10, 12, 15, 16, 19, (22)
+    (0), 1, 4, 7, 10, 11, 12, 15, 16, 19, (22)
+    (0), 1, 4, 7, 10, 12, 15, 16, 19, (22)
+
+(The charging outlet and your device's built-in adapter are shown in
+parentheses.) Given the adapters from the first example, the total number of
+arrangements that connect the charging outlet to your device is 8.
+
+The second example above (the one that starts with 28, 33, 18) has many
+arrangements. Here are a few:
+
+    (0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+    32, 33, 34, 35, 38, 39, 42, 45, 46, 47, 48, 49, (52)
+
+    (0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+    32, 33, 34, 35, 38, 39, 42, 45, 46, 47, 49, (52)
+
+    (0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+    32, 33, 34, 35, 38, 39, 42, 45, 46, 48, 49, (52)
+
+    (0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+    32, 33, 34, 35, 38, 39, 42, 45, 46, 49, (52)
+
+    (0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+    32, 33, 34, 35, 38, 39, 42, 45, 47, 48, 49, (52)
+
+    (0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+    46, 48, 49, (52)
+
+    (0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+    46, 49, (52)
+
+    (0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+    47, 48, 49, (52)
+
+    (0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+    47, 49, (52)
+
+    (0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+    48, 49, (52)
+
+In total, this set of adapters can connect the charging outlet to your device
+in 19208 distinct arrangements.
+
+You glance back down at your bag and try to remember why you brought so many
+adapters; there must be more than a trillion valid ways to arrange them!
+Surely, there must be an efficient way to count the arrangements.
+
+What is the total number of distinct ways you can arrange the adapters to
+connect the charging outlet to your device?
+
+* 12089663946752.
 """
 
 
@@ -120,21 +192,32 @@ def format_data(data):
     return [int(x) for x in data.readlines()]
 
 
-def check_adapters(adapters, input_joltage):
-    valid_chain = []
+def check_adapters(adapters):
     differences = {0: 0, 1: 0, 2: 0, 3: 0}
-    for joltage in adapters:
-        diff = joltage - input_joltage
+    for i in range(1, len(adapters)):
+        diff = adapters[i] - adapters[i-1]
         if diff <= 3:
             differences[diff] += 1
-            valid_chain.append(joltage)
-            input_joltage = joltage
+            input_joltage = adapters[i]
     return differences[1] * differences[3]
+
+
+def find_all_combinations(i, adapters, seen):
+    if i == len(adapters)-1:
+        return 1
+    if i in seen:
+        return seen[i]
+    ans = 0
+    for j in range(i + 1, len(adapters)):
+        if adapters[j] - adapters[i] <= 3:
+            ans += find_all_combinations(j, adapters, seen)
+    seen[i] = ans
+    return ans
 
 
 if __name__ == "__main__":
     with open("Day_10/input.txt", "r") as in_file:
         joltages = format_data(in_file)
-        joltages.sort()
-        joltages.append(joltages[-1]+3)
-        print(check_adapters(joltages, 0))
+        joltages = sorted([0, *joltages, max(joltages)+3])
+        print(check_adapters(joltages))
+        print(find_all_combinations(0, joltages, {}))
